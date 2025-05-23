a5b5ea89ad5753a2a8857b2ba9588c45586007ad
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-05-23 20:18:35 +0200

    Update rfcom.py

--- rfcom.py
+++ rfcom.py
@@ -5,7 +5,7 @@ import re
 import os
 
 RUTA_FICHERO = "/home/pi/.local/bluetooth.sh"
-linea_actual = 2  # contador de líneas a escribir
+linea_actual = 3  # contador de líneas a escribir
 
 def escribir_comando_en_fichero(mac):
     global linea_actual
