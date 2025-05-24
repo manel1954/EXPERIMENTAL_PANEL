0f0a466a4ec362414adcc881ade9e3ab20adf8b9
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-05-25 00:36:19 +0200

    Update rfcom.py

--- rfcom.py
+++ rfcom.py
@@ -227,7 +227,8 @@ frame_escaneo.pack(fill="both", expand=True, padx=10, pady=5)
 btn_script = tk.Button(root, text="Ejecutar script completo", command=ejecutar_script_completo,
                        bg="#17a2b8", fg="white", font=("Arial", 10, "bold"),
                        bd=0, highlightthickness=0, relief="flat")
-btn_script.pack(pady=5, anchor="w", padx=10)
+#btn_script.pack(pady=5, anchor="w", padx=10)
+btn_script.pack(fill="x", expand=True, padx=10)
 
 
 
