9398c68cf90d08fc1cff9a7dc4b31f54cfefd39b
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-05-24 22:24:34 +0200

    Update rfcom.py

--- rfcom.py
+++ rfcom.py
@@ -201,7 +201,7 @@ def ejecutar_script_completo():
 
 root = tk.Tk()
 root.title("Gestión Bluetooth rfcomm")
-root.geometry("445x485+1287+550")
+root.geometry("448x485+1287+550")
 root.configure(bg="#121212")
 
 tk.Button(root, text="Refrescar lista vinculados", command=refrescar_lista,
