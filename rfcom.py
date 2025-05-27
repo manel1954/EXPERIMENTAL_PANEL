d6e6e369eb1cf0e03c19ef9c54ea2b5ced3ad58a
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-05-27 23:57:47 +0200

    Update rfcom.py

--- rfcom.py
+++ rfcom.py
@@ -202,7 +202,7 @@ def abrir_formulario_importante():
     ventana = tk.Toplevel(root)
     ventana.title("descripcion")
     ventana.configure(bg="#0E0A5C")
-    ventana.geometry("500x485+1448+69")
+    ventana.geometry("570x485+1448+69")
 
     texto = """\
           
