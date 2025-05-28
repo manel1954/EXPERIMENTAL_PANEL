c4f29401d272066b06ede2e395413ce6fce4168e
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-05-28 09:31:26 +0200

    Update rfcom.py

--- rfcom.py
+++ rfcom.py
@@ -321,17 +321,17 @@ def actualizar_botones_bluetooth():
 frame_bluetooth = tk.Frame(root, bg="#121212")
 frame_bluetooth.pack(pady=5)
 
-btn_bt_activado = tk.Button(frame_bluetooth, text="Bluetooth Activado",
+btn_bt_activado = tk.Button(frame_bluetooth, text="Bluetooth Desactivado",
                             command=lambda: cambiar_estado_bluetooth(True),
                             font=("Arial", 10, "bold"),
-                            fg="white", bg="green",
+                            fg="white", bg="#dc3545",
                             bd=0, highlightthickness=0)
 btn_bt_activado.pack(side="left", padx=5)
 
-btn_bt_desactivado = tk.Button(frame_bluetooth, text="Bluetooth Desactivado",
+btn_bt_desactivado = tk.Button(frame_bluetooth, text="Bluetooth Activado",
                                command=lambda: cambiar_estado_bluetooth(False),
                                font=("Arial", 10, "bold"),
-                               fg="white", bg="#dc3545",
+                               fg="white", bg="green",
                                bd=0, highlightthickness=0)
 btn_bt_desactivado.pack(side="left", padx=5)
 
