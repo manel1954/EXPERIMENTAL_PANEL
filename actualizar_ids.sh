99ed0576328f201f58a57f3ff01050cf71e4be07
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-06-22 13:01:35 +0200

    Update actualizar_ids.sh

--- actualizar_ids.sh
+++ actualizar_ids.sh
@@ -29,10 +29,10 @@
                         cp DMRIds.dat /home/pi/YSF2DMR/
                         sleep 5
                         
-                        # Cambio realizado el 14-04-2025 para actualizar los IDS en MMDVMHost
+                        # Cambio realizado el 14-04-2025 para actualizar los IDS en MMDVMHost 
                         cd /home/pi/MMDVMHost
                         sudo curl --fail -o DMRIds.dat -s http://www.pistar.uk/downloads/DMRIds.dat
-                        cp DMRIds.dat /home/pi/MMDVMHost/
+                        #cp DMRIds.dat /home/pi/MMDVMHost/
                         sudo chmod 777 /home/pi/MMDVMHost/DMRIds.dat
 
                         
