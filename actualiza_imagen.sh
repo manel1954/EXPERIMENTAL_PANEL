d86defccf0665fdbffff010e8d836ea70bb1b346
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-06-27 18:19:33 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -14,7 +14,7 @@ sed -i "2c $fecha_formateada" /home/pi/version-fecha-actualizacion
                         sudo chmod 777 -R /home/pi/A108
                         
                         sudo rm -R /home/pi/esp32/esp32_firmware 
-                        cp -R /home/pi/A108/esp32 /home/pi/
+                        sudo cp -R /home/pi/A108/esp32 /home/pi/
                         sudo chmod +x -R /home/pi/esp32
 
                         #cp /home/pi/A108/Abrir_Paneles.desktop /home/pi/.config/autostart
