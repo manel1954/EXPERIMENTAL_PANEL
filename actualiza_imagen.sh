c98d8edb7ed5a621eaff23af827167c736eb20d6
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-07-02 15:38:59 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -13,7 +13,7 @@ sed -i "2c $fecha_formateada" /home/pi/version-fecha-actualizacion
                         sleep 6                                             
                         sudo chmod 777 -R /home/pi/A108
                         
-                        sudo rm -R /home/pi/esp32/esp32_firmware 
+                        sudo rm -R /home/pi/esp32 
                         sudo cp -R /home/pi/A108/esp32 /home/pi/
                         sudo chmod +x -R /home/pi/esp32
 
